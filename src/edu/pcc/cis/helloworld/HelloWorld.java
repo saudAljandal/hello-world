153bdabb801e834b68087867aaac12fcba419d9a
Aljandal Saud <saud.aljandal@pcc.edu>
2017-07-24 00:00:33 -0700

    print name

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,8 +1,13 @@
+/*
+Aljandal,Saud
+ */
+
+
 package edu.pcc.cis.helloworld;
 import java.util.Date;
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World. This is Saud Aljandal");
         System.out.println(new Date());
     }
 }
