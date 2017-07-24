427e761775e69485cdbde34f026cfbbc9cee92b9
Aljandal Saud <saud.aljandal@pcc.edu>
2017-07-24 00:24:36 -0700

    head tail game

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -5,9 +5,19 @@ Aljandal,Saud
 
 package edu.pcc.cis.helloworld;
 import java.util.Date;
+
+
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World. This is Saud Aljandal");
         System.out.println(new Date());
+
+        //simple heads tails game. Operates by Math.random()
+        if (Math.random() < 0.5)
+            System.out.println("Heads");
+        else
+            System.out.println("Tails");
+
+
     }
 }
